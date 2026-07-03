8c5fb5b14cda6a8d47591b278c196fb7b5e7531f
OMID <omidsibilant0910-lang>
2026-07-03 18:16:12 +0330

    add coin 0

--- App.js
+++ App.js
@@ -2,11 +2,25 @@ const hamsterBtn = document.querySelector("#hamsterBtn")
 hamsterBtn.addEventListener("click",function () {
     console.log(5)
 })
+
+
 const coinValeu= document.querySelector("#coinValue")
 console.log(coinValeu)
+
+
 function setDefaultRatio() {
     if (localStorage.getItem("ratio") === null) {
         localStorage.setItem("ratio", "1")
     }
 }
 setDefaultRatio()
+
+function setDefaultCoins () {
+    if (localStorage.getItem("coins") === null) {
+        localStorage.setItem("coins" , "0")
+    }}
+    setDefaultCoins()
+
+
+
+
