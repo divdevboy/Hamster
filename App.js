e906f945d77fc1e728ee43ffac49edd665be202a
arash <arashdivdevboy>
2026-07-03 17:53:14 +0330

    added default ratio

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
+function setDefaultRatio() {
+    if (localStorage.getItem("ratio") === null) {
+        localStorage.setItem("ratio", "1")
+    }
+}
+setDefaultRatio()
 const hamsterBtn = document.querySelector("#hamsterBtn")
 hamsterBtn.addEventListener("click",function () {
-    console.log(5)
 })
