701286cdb89f8ca7f67a8a5bd05e07ee93c3a7ea
OMID <omidsibilant0910-lang>
2026-07-03 17:52:12 +0330

    connected coinValue

--- App.js
+++ App.js
@@ -2,3 +2,5 @@ const hamsterBtn = document.querySelector("#hamsterBtn")
 hamsterBtn.addEventListener("click",function () {
     console.log(5)
 })
+const coinValeu= document.querySelector("#coinValue")
+console.log(coinValeu)
