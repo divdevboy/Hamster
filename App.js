9a63211050c4bb5820658eb25641cbf94d241ab4
eisa2008 <eisatajik2008@gmail.com>
2026-07-24 11:18:16 +0330

    eisa was added

--- App.js
+++ App.js
@@ -2,9 +2,7 @@
 import Mbn from "./utils/Mbn.js";
 import Omd from "./utils/Omd.js";
 import Arash from "./utils/Arash.js";
-
 const coinValue= document.querySelector("#coinValue")
-console.log(coinValue)
 const hamsterBtn = document.querySelector("#hamsterBtn")
 const mbn = new Mbn()  // define new mbn class
 const omd = new Omd ()
@@ -12,20 +10,14 @@ const arash = new Arash()
 hamsterBtn.addEventListener("click",function () {
     arash.sum(mbn.getCoins(),omd.getRatio())
 })
-
-
-
-
 //This code finds the coinValue element and prints it in the console//
 mbn.setDefaultRatio()
-
-
 // function set default coins 0 if there aren't saved coins
     mbn. setDefaultCoins()
 
 
 
-//This code get ratio from localstorage and return it//
+
 
 
 
