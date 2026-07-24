3bedf35a52e4a3b56fcb187adfb1631d73f25e82
eisa2008 <eisatajik2008@gmail.com>
2026-07-24 12:14:20 +0330

    nut event listener added

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import Omd from "./utils/Omd.js";
 import Arash from "./utils/Arash.js";
 const coinValue= document.querySelector("#coinValue")
 const hamsterBtn = document.querySelector("#hamsterBtn")
+const nut = document.querySelector("#nut")
 const mbn = new Mbn()  // define new mbn class
 const omd = new Omd ()
 const arash = new Arash()
 hamsterBtn.addEventListener("click",function () {
     arash.sum(mbn.getCoins(),omd.getRatio())
+})
+nut.addEventListener("click",function (){
 })
 //This code finds the coinValue element and prints it in the console//
 mbn.setDefaultRatio()
