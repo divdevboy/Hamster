99dc686937cccdc1a1313a412c117d7dd2e36621
arash <arashdivdevboy>
2026-07-24 13:17:51 +0330

    bug was fixed and another bug was appeared

--- App.js
+++ App.js
@@ -13,14 +13,17 @@ hamsterBtn.addEventListener("click",function () {
     arash.sum(mbn.getCoins(),omd.getRatio())
 })
 nut.addEventListener("click",function (){
-   arash.buyIfAfford()
+    let nutPrice =  localStorage.getItem("nutPrice")
+   arash.buyIfAfford(mbn.getCoins(),nutPrice)
 })
 //This code finds the coinValue element and prints it in the console//
 mbn.setDefaultRatio()
 // function set default coins 0 if there aren't saved coins
     mbn. setDefaultCoins()
+// show saved price for nut
 arash.sum(mbn.getCoins(),"0")
-
+// set default price for nut
+omd.setDefaultNutPrice()
 
 
 
