009096f6d79b2e26f898c2562e20c9978bf71719
arash <arashdivdevboy>
2026-08-07 14:51:53 +0330

    update click power ui

--- App.js
+++ App.js
@@ -27,3 +27,4 @@ omd.setDefaultNutPrice()
 
 
 
+
--- utils/Arash.js
+++ utils/Arash.js
@@ -1,10 +1,13 @@
 import Eisa from "./Eisa.js";
 import Omd from "./Omd.js";
-export default class Arash{
-    constructor(){}
+export default class Arash {
+    constructor() {
+    }
+
     eisa = new Eisa()
     omd = new Omd()
-    sum (coins,ratio) {
+
+    sum(coins, ratio) {
         let total = Number(coins) + Number(ratio)
         coinValue.innerHTML = total
         localStorage.setItem("coins", total)
@@ -12,29 +15,36 @@ export default class Arash{
     }
 
 // the function show 2 number, total on the page and saved in localstorage
-buyIfAfford (coins,nutPrice,coinElement) {
-    console.log("تازه وارد فانکشن شدیم" , coins, nutPrice)
-    if (Number(coins)>=Number(nutPrice)){
+    buyIfAfford(coins, nutPrice, coinElement) {
+        console.log("تازه وارد فانکشن شدیم", coins, nutPrice)
+        if (Number(coins) >= Number(nutPrice)) {
             console.log("تازه اول if", coins, nutPrice)
             let result = Number(coins) - Number(nutPrice)
-        console.log("نتیجه تفریق" , result)
-           this.eisa.updateNutPriceText(result)
+            console.log("نتیجه تفریق", result)
+            this.eisa.updateNutPriceText(result)
             this.omd.setDefaultNutPrice(result)
-            localStorage.setItem("coins",result.toString())
-            this.omd.updateCoinUi(result,coinElement)
-        this.updateRatio()
-
-        this.eisa.updateNutPrice1()
+            localStorage.setItem("coins", result.toString())
+            this.omd.updateCoinUi(result, coinElement)
+            this.updateRatio()
+            this.eisa.updateNutPrice1()
+            this.updateClickPowerUi()
         } else {
             window.alert("سکه کافی نمیباشد")
         }
 
-}
+    }
 
-    updateRatio () {
+    updateRatio() {
         let ratio = localStorage.getItem("ratio")
-        let newRatio = Number(ratio) * 2
+        let newRatio = Number(ratio) +1
         localStorage.setItem("ratio", newRatio.toString())
-}
-}
+    }
 
+    updateClickPowerUi() {
+        const clickPower = document.querySelector("#clickPower")
+        let showClick = localStorage.getItem("ratio")
+        clickPower.innerHTML = showClick
+
+
+    }
+}
